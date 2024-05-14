a79e0364e1ca52d9ac950452dadce4efce804051
shilong <shilong>
2024-05-14 14:46:03 +0800

    modify data organization

--- scripts/v1_5/finetune_continue_sft.sh
+++ scripts/v1_5/finetune_continue_sft.sh
@@ -1,23 +1,23 @@
 port=$(shuf -i25000-30000 -n1)
 
-data_path='your_data_path'
+data_path='data'
 
 HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1  deepspeed --include localhost:0,1,2,3,4,5,6,7  --master_port $port videollava/train/train_mem.py \
     --deepspeed ./scripts/zero2_offload.json \
     --model_name_or_path checkpoints/Video-LLaVA-7B-split  \
     --version v1 \
     --data_path ./scripts/v1_5/data_list.json \
-    --image_folder ${data_path} \
+    --image_folder ${data_path}/image \
     --image_tower LanguageBind_Image \
-    --audio_folder ${data_path} \
+    --audio_folder ${data_path}/audio \
     --audio_tower LanguageBind_Audio \
-    --video_folder ${data_path} \
+    --video_folder ${data_path}/video \
     --video_tower LanguageBind_Video_merge \
-    --depth_folder ${data_path} \
+    --depth_folder ${data_path}/depth \
     --depth_tower LanguageBind_Depth \
-    --thermal_folder ${data_path} \
+    --thermal_folder ${data_path}/thermal \
     --thermal_tower LanguageBind_Thermal \
-    --electromagnetic_wave_folder ${data_path} \
+    --electromagnetic_wave_folder ${data_path}/electromagnetic_wave \
     --mm_projector_type mlp2x_gelu \
     --pretrain_mm_mlp_adapter checkpoints/Video-LLaVA-7B-split/mm_projector.bin \
     --mm_vision_select_layer -2 \
